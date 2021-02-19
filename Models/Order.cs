b297ec52a6d4c0b581dbc8894f94d59748b1e3a1
Lucas Herrmann <Herminator615@hotmail.com>
2021-02-19 15:10:16 -0700

    Added model classes for Order, OrderProduct, Product, and User. Added DBContext called StoreContext.cs.

---
+++ Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Test_API.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public decimal TotalCost { get; set; }
+        public string DeliveryAddress { get; set; }
+        public string DeliveryPhone { get; set; }
+        public ICollection<User> UserID { get; set; }
+        public ICollection<Product> ProductID { get; set; }
+    }
+}
