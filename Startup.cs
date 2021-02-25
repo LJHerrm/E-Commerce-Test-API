1389d456e8e9184637db4b7a01e97e393097b754
Lucas Herrmann <Herminator615@hotmail.com>
2021-02-25 15:25:06 -0700

    Configured the StoreContext as a service in Startup.cs and added DefaultConnection string to appsettings.json

--- Startup.cs
+++ Startup.cs
@@ -7,15 +7,33 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using E_Commerce_Test_API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace E_Commerce_Test_API
 {
     public class Startup
     {
+        //Added to access Configuration.GetConnectionString, which cannot be accessed in ConfigureServices method otherwise
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Gets connection information from appsettings.json DefaultConnection field
+            services.AddDbContext<StoreContext>(options =>
+               options.UseNpgsql(_configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
