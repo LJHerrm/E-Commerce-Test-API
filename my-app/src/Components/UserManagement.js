47706ea0955765c7f33a760271991d22745efcff
Lucas Herrmann <Herminator615@hotmail.com>
2021-04-22 16:19:36 -0600

    More work on User Management page:      added "add user" button and modal      added table that displays the users db table      added edit and delete buttons to table

--- my-app/src/Components/UserManagement.js
+++ my-app/src/Components/UserManagement.js
@@ -1,19 +1,31 @@
 ﻿import React from 'react';
+import Modal from './Modal.js';
 
 export default class UserManagement extends React.Component {
+
     constructor(props) {
         super(props);
         this.state = {
+            show: false,
             error: null,
             isLoaded: false,
             users: [],
-            name: null,
+            name: "",
             role: null,
-            email: null,
+            email: "",
             newUserID: null
-        };
-    }
 
+        };
+    }        
+
+    //set state to show/hide modal
+    showModal = e => {
+        this.setState({
+            show: !this.state.show
+        });
+    };
+
+    //Handlers for updating states when forms are filled
     updateName(event) {
         this.setState({
             name: event.target.value
@@ -55,28 +67,44 @@ export default class UserManagement extends React.Component {
     }
 
     //Send a post request to /api/users and store the new ID in newUserID
-    AddNewUser() {
+
+    //CreatedAt is currently disabled until it is figured out
+    //'CreatedAt': Date().toLocaleString()
+    async AddNewUser() {
+
         const postRequest = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                'Name': this.state.Name,
-                'Role': this.state.Role,
-                'Email': this.state.Email,
-                'CreatedAt': Date().toLocaleString()})
+                'Name': this.state.name,
+                'Role': this.state.role,
+                'Email': this.state.email,
+                
+                })
         };
-        fetch("http://localhost:5000/api/Users", postRequest)
-            .then(response => response.json())
-            .then(result => this.setState({ newUserID: result.UserID }));
-    }
 
+        const response = await fetch('http://localhost:5000/api/Users', postRequest);
+        const data = await response.json();
+        this.setState({ newUserID: data.id });
+
+        //Log new user ID to console
+        await console.log('New user added with ID:');
+        await console.log(this.state.newUserID);
+
+        //Refresh the user list and close modal
+        await this.GetAllUsers();
+        await this.showModal();
+    }
+    
     componentDidMount() {
 
         this.GetAllUsers();
         
     }
-
+     
     render() {
+        
+
         const { error, isLoaded, users } = this.state;
         if (error) {
             return <div>Error: {error.message}</div>;
@@ -84,14 +112,54 @@ export default class UserManagement extends React.Component {
             return <div>Loading...</div>;
         } else {
             return (
-                <ul>
-                    {users.map(user => (
-                        <li key={user.userID}>
-                            {user.name} {user.role} {user.email} {user.createdAt}
-                        </li>
-                    ))}
-                </ul>
+                <div>
+                    <h2> Users </h2>
+                    <button onClick = {e => { this.showModal(e); }}> Add New User </button>
+
+                    <Modal onClose = {this.showModal} show = {this.state.show} >
+                        Name: 
+                        <input type = "text" value = {this.state.name} onChange = {this.updateName.bind(this)} />
+                        Role:
+                        <input type = "text" value = {this.state.role} onChange = {this.updateRole.bind(this)} />
+                        Email:
+                        <input type = "text" value = {this.state.email} onChange = {this.updateEmail.bind(this)} />
+                        <button onClick = {this.AddNewUser.bind(this)} > Save </button>
+                    </Modal>
+                    
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>User ID</th>
+                                <th>Name</th>
+                                <th>Role</th>
+                                <th>Email</th>
+                                <th>Time/Date Created</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {users.map(user => (
+                                <tr>
+                                    <td> {user.userID} </td>
+                                    <td> {user.name} </td>
+                                    <td> {user.role} </td>
+                                    <td> {user.email} </td>
+                                    <td> {user.createdAt} </td>
+                                    <td> <button onClick={e => { this.showModal(e); }}> Edit </button> </td>
+                                    <td> <button onClick={e => { this.showModal(e); }}> Delete </button> </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+               </div>
             );
         }
     }
 }
+
+/* <ul>
+    {users.map(user => (
+        <li key={user.userID}>
+            {user.name} {user.role} {user.email} {user.createdAt}
+        </li>
+    ))}
+</ul> */
