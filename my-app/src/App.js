bfc17ff26cd7d8a2af043832ca4054b2d895ead5
Lucas Herrmann <Herminator615@hotmail.com>
2021-07-12 13:48:39 -0600

    Installed @material-ui/icons and modified App.js to have a top toolbar that contains Search bar and Account/Cart buttons. The Account button brings up a menu when clicked with various options. Functionality not implemented yet.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,86 @@
 import './App.css';
 import React from 'react';
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserManagement from './Components/UserManagement.js';
 import AdminPage from './Components/Admin.js';
 import ProductManagement from './Components/ProductManagement.js';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        width: 600,
+    },
+    input: {
+        marginLeft: theme.spacing(1),
+        flex: 1,
+    },
+}));
 
 export default function BasicRouter() {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleAccountClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleAccountClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <Router>
             <div>
+                <ul>
+                    <Grid container direction="row" alignItems="center" spacing={2}>
+                        <Grid item>
+                            <Paper component="form" className={classes.root}>
+                                <InputBase
+                                    className={classes.input}
+                                    placeholder="Search Products"
+                                    inputProps={{ 'aria-label': 'search products' }}
+                            />
+                                <IconButton type="submit"  aria-label="search">
+                                    <SearchIcon />
+                                </IconButton>
+                            </Paper>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" color="primary" onClick={handleAccountClick}> Account </Button>
+                            <Menu
+                                id="login-menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleAccountClose}
+                            >
+                                <MenuItem onClick={handleAccountClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleAccountClose}>My account</MenuItem>
+                                <MenuItem onClick={handleAccountClose}>Logout</MenuItem>
+                            </Menu>
+                        </Grid>
+                        <Grid item>
+                            <Button variant="contained" color="primary"> Cart </Button>
+                        </Grid>
+                    </Grid>
+                </ul>
+                
+                <hr />
+
                 <ul>
                     <Breadcrumbs>
                         <Link color="inherit" href="/home">Home</Link>
@@ -25,16 +89,14 @@ export default function BasicRouter() {
                         <Link href="/admin/products">Product Management Page</Link>
                     </Breadcrumbs>
                 </ul>
-
                 <hr />
-
                 {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
+                A <Switch> looks through all its children <Route>
+                elements and renders the first one whose path
+                matches the current URL. Use a <Switch> any time
+                you have multiple routes, but you want only one
+                of them to render at a time
+                */}
                 <Switch>
                     <Route exact path="/">
                         <Home />
@@ -47,8 +109,7 @@ export default function BasicRouter() {
                     </Route>
                     <Route path="/admin">
                         <AdminPage />
-                    </Route>
-                    
+                    </Route>  
                 </Switch>
             </div>
         </Router>
